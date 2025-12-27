7f4f5803cfa502e283503ec1b932faaf57f24f52
Radio <100038018+Radio2840@users.noreply.github.com>
2025-12-27 16:15:06 +0100

    Usunięcie if
    if który nie miał sensu

--- main.py
+++ main.py
@@ -40,8 +40,7 @@ class MyForm(QDialog):
         for list_widget in list_widgets:
             list_of_items_str = []
             items_count = list_widget.count()
-            if items_count == 0:
-                break
+
             for i in range(items_count):
                 current_item = list_widget.item(i)
                 list_of_items_str.append(current_item.text())
@@ -64,7 +63,6 @@ class MyForm(QDialog):
             
             for list_widget in list_widgets:
                 selected_items = list_widget.selectedItems()
-                
                 for item in selected_items:
                     row = list_widget.row(item)
                     list_widget.takeItem(row) 
